9fc321672d6c43fa2e47fdf92152f7c20fd48ef5
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-09-17 14:52:36 +0300

    solution task DuplicateEncode

---
+++ CodeWars/KataTask/DuplicateEncodeKata.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace CodeWars.KataTask;
+
+public class DuplicateEncodeKata
+{
+    /// <summary>
+    /// Задача - преобразовать строку в новую строку, где каждый символ в новой строке является,
+    /// "(" если этот символ появляется только один раз в исходной строке,
+    /// или ")" если этот символ появляется в исходной строке более одного раза.
+    /// Ссылка на задание: https://www.codewars.com/kata/54b42f9314d9229fd6000d9c
+    /// </summary>
+    /// <param name="word">Исходная строка</param>
+    /// <returns>Закодированная строка</returns>
+    public static string DuplicateEncode(string word)
+    {
+        var codeWord = new StringBuilder();
+
+        var result = word.ToLower().Select(letter => letter.ToString()).ToArray();
+
+        foreach (var letter in result)
+        {
+            var letterIsRepeat = result.Count(c => c == letter) > 1;
+
+            codeWord.Append(letterIsRepeat ? ")" : "(");
+        }
+
+        return codeWord.ToString();
+    }
+}
